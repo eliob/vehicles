7cf2cd345164441830e9f9fbb57df72d87bbeeb4
Amir <amir.katorza@gmail.com>
2021-10-27 14:43:26 +0300

    delete junk code

--- main.py
+++ main.py
@@ -2,13 +2,6 @@ import pandas as pd
 import download_kaggle_dataset
 import utils
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     download_kaggle_dataset.download_dataset('austinreese/craigslist-carstrucks-data')
     cars = pd.read_csv('data/vehicles.csv')
